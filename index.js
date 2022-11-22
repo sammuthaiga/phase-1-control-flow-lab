506dde943e4515f02c790b7a984af84a176af1a9
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-22 20:55:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if (feet > 2500) {
+  return 'No can do.'
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet <= 400) {
+    return 'This one is on me!';
+  }
+}
+ 
+  
+
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  const goToCity = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  return goToCity;
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (tips) {
+    case 'generous':
+      return "Thank you so much.";
+    
+    case 'not as generous':
+      return "Thank you.";
+    
+    default :
+      return "Bye.";
+     
+  }
+  
+    
+      
+  }
--- test/indexTest.js
+++ test/indexTest.js
@@ -17,6 +17,7 @@ describe('index.js', function () {
     it('returns "Ok, sounds good." when the city is NYC', function () {
       expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
     });
+    
 
     it('should return "No go." if the destination city is not NYC', function () {
       expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
